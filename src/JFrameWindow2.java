6de5592a0bbafc36990cdb0e5814f3c9a5652095
ZZY <oncwnuNI61-s8pz054wJG2--S4GA@git.weixin.qq.com>
2022-07-02 17:30:47 +0800

    画好窗口了

--- src/JFrameWindow2.java
+++ src/JFrameWindow2.java
@@ -12,8 +12,12 @@ public class JFrameWindow2 extends JFrame{
     JPanel jp1, jp2, jp3;
     JLabel jlb1, jlb2;
     JButton jb1, jb2, jb3, jb4;
-    JTextPane url;
+    JScrollPane scrollPane;
+    JEditorPane text;
     JTextField sleepTime;
+
+    int format_width = 800;
+    int format_height = 800;
 
     public static void createWindow() {
         // TODO Auto-generated method stub
@@ -23,61 +27,75 @@ public class JFrameWindow2 extends JFrame{
 
     // 构造函数
     public JFrameWindow2() {
-
         try {
             InputStream imgIS = this.getClass().getResourceAsStream("z.png");
             Image image = ImageIO.read(imgIS);
             this.setIconImage(image);
-
-        }catch (Exception e){
-
-        }
-
-
+        }catch (Exception e){ }
         setResizable(false);
-        setLocation(getToolkit().getScreenSize().width/3, getToolkit().getScreenSize().height/3);
-        setSize(500, 180);
+        setLocation(getToolkit().getScreenSize().width/4, getToolkit().getScreenSize().height/100);
+        setSize(1000, 1000);
 
         jp1 = new JPanel();
+        //jp1.setSize(format_width,500);
         jp2 = new JPanel();
+        //jp2.setSize(800,800);
         jp3 = new JPanel();
+        //jp3.setSize(format_width,500);
 
         jlb1 = new JLabel("简谱内容");
 
-        jb1 = new JButton("开始");
-        jb2 = new JButton("停止");
-        jb3 = new JButton("试听");
+        jb1 = new JButton("开始录制");
+        jb2 = new JButton("停止录制");
+        jb3 = new JButton("开始试听");
+        jb4 = new JButton("停止试听");
 
-        url = new JTextPane();
+
+
+
+        text = new JEditorPane();
+        text.setSize(800,800);
+        text.setPreferredSize(new Dimension(800,850));
+
+
+        scrollPane = new JScrollPane(text);
+        //scrollPane.setPreferredSize(new Dimension(800,800));
+        scrollPane.setHorizontalScrollBarPolicy(ScrollPaneConstants.HORIZONTAL_SCROLLBAR_NEVER);
+        scrollPane.setVerticalScrollBarPolicy(JScrollPane.VERTICAL_SCROLLBAR_AS_NEEDED);
+
+
         sleepTime = new JTextField(30);
 
 
         Font font = new Font("宋体",Font.PLAIN,22);
-        url.setFont(font);
+        text.setFont(font);
         sleepTime.setFont(font);
         jlb1.setFont(font);
         jb1.setFont(font);
         jb2.setFont(font);
         jb3.setFont(font);
+        jb4.setFont(font);
 
-        GridLayout gl = new GridLayout(3, 1);
-        this.setLayout(gl);
+        //GridLayout gl = new GridLayout(3, 1);
+        //this.setLayout(gl);
 
         // 加入各个组件
+        //第一行
         jp1.add(jlb1);
 
-        jp2.add(url);
+        //第二行
+        jp2.add(scrollPane);
 
-
+        //第三行
         jp3.add(jb1);
         jp3.add(jb2);
         jp3.add(jb3);
-
+        jp3.add(jb4);
 
         // 加入到JFrame
-        this.add(jp1);
-        this.add(jp2);
-        this.add(jp3);
+        this.add(jp1,BorderLayout.NORTH);
+        this.add(jp2,BorderLayout.CENTER);
+        this.add(jp3,BorderLayout.SOUTH);
 
         //setListener(jb1, jb2, url, sleepTime);
 
